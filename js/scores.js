4d46e4a3787fc58045236b4029d0a063fcf03f24
hkhawaja3 <hkhawaja3@gmail.com>
2018-04-14 17:51:23 -0500

    updated

--- js/scores.js
+++ js/scores.js
@@ -6,3 +6,30 @@ $('.open-intro').click(function() {
 $('.close-intro').click(function() {
     $('#intro-wrap').slideUp();
 });
+
+$('.open-intro2').click(function() {
+    $('#intro-wrap2').slideDown();
+});
+
+
+$('.close-intro2').click(function() {
+    $('#intro-wrap2').slideUp();
+});
+
+$('.open-intro3').click(function() {
+    $('#intro-wrap3').slideDown();
+});
+
+
+$('.close-intro3').click(function() {
+    $('#intro-wrap3').slideUp();
+});
+
+$('.open-intro4').click(function() {
+    $('#intro-wrap4').slideDown();
+});
+
+
+$('.close-intro4').click(function() {
+    $('#intro-wrap4').slideUp();
+});
